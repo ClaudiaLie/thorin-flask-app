7a9cc9f0dd2c51a6f0cb12f02778d870e9bd052a
ClaudiaLie <claudiss.lai@gmail.com>
2021-05-24 17:51:15 +0000

    Adding data for the About page

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
@@ -23,8 +25,8 @@ def careers():
     return render_template("careers.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port = int(os.environ.get("PORT", "5000")),
-        debug = True) 
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
